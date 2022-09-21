4d1867c868466417558aeb9ab2315433cf4de6fb
scrungus <tyler@stackhpc.com>
2022-09-21 10:12:59 +0000

    added missing required classmethods

--- HealthMonitor_tempest_plugin/tests/api/test_basic.py
+++ HealthMonitor_tempest_plugin/tests/api/test_basic.py
@@ -1,13 +1,26 @@
 import tempest.test
 from HealthMonitor_tempest_plugin.common.manager import Manager 
+import testtools
 
 class BasicTest(tempest.test.BaseTestCase):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super(BasicTest,self).__init__(*args,**kwargs)
+
+    @testtools.testcase.attr('positive')
+    def test_basic(self):
         self.manager = Manager()
         print("I authenticated!")
-        exit()
+        self.assertEqual('yes','yes');
 
-    def skip_checks(self):
-        super().skip_checks()
+    @classmethod
+    def skip_checks(cls):
+        super(BasicTest,cls).skip_checks()
+
+    @classmethod
+    def resource_setup(cls):
+        super(BasicTest, cls).resource_setup()
+
+    @classmethod
+    def resource_cleanup(cls):
+        super(BasicTest, cls).resource_cleanup()
