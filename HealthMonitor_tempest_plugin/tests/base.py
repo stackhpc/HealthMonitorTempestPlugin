3f3207c6a7578ac8112fc81232e715217ebdae04
scrungus <tyler@stackhpc.com>
2022-09-23 12:54:09 +0000

    moved ssh + create server logic into base test

--- HealthMonitor_tempest_plugin/tests/base.py
+++ HealthMonitor_tempest_plugin/tests/base.py
@@ -4,10 +4,26 @@ from tempest import config
 
 import ipaddress
 
+from tempest.lib.common.utils import data_utils
+
+from tempest.lib.common.utils.linux import remote_client 
+
+import sys 
+
+import time
+
 from HealthMonitor_tempest_plugin.common.manager import Manager 
 
 CONF = config.CONF
+
+#setup logging to output to console
 LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
 
 class BaseHealthCheck(tempest.test.BaseTestCase):
 
@@ -59,3 +75,95 @@ class BaseHealthCheck(tempest.test.BaseTestCase):
                         % port_map)
 
         return port_map[0]
+
+    def create_server_and_get_ssh(self,image,flavor):
+
+        SERVER_NAME = data_utils.rand_name(name='server',prefix='healthmon')
+        KEYPAIR_NAME = data_utils.rand_name(name='keypair',prefix='healthmon')
+
+        r = self.manager.keypairs_client.create_keypair(name=KEYPAIR_NAME)
+        pk = r['keypair']['private_key']
+        pubk = r['keypair']['public_key']
+        LOG.info("Generated SSH keypair '%s'",KEYPAIR_NAME)
+        LOG.info("pub_key: %s",pubk)
+        
+        LOG.info('Creating server...')
+        t1 = time.perf_counter()
+        r = self.manager.servers_client.create_server(name=SERVER_NAME, imageRef=image, flavorRef=flavor, key_name=KEYPAIR_NAME, networks=[{'uuid': CONF.network.public_network_id}]);
+        t2 = time.perf_counter()
+
+        id = r['server']['id']
+
+        LOG.info('server_name : %s server_id : %s',SERVER_NAME,id)
+        LOG.info('---------------------------')
+        LOG.info('Time to create server : {:3.4f}s'.format(t2-t1))
+        
+        LOG.info('Waiting for server to become active...')
+        t1 = time.perf_counter()
+        while True:
+            r = self.manager.servers_client.show_server(id)
+            LOG.info("Polling...")
+            LOG.info('STATUS: %s',r['server']['status'])
+            if r['server']['status'] == "ACTIVE":
+                t2 = time.perf_counter()
+                tenant_id = r['server']['tenant_id']
+                break
+            else:
+                time.sleep(CONF.compute.ready_wait)
+        LOG.info("Got server status ready")
+        LOG.info('Time to complete (note: this time may be affected by the polling period "ready_wait" configured in tempest (/etc/tempest.conf)): {:3.4f}s'.format(t2-t1))
+        LOG.info('---------------------------')
+        
+        LOG.info("Attempting to ssh into server...")
+        LOG.info("Getting ports for server with id: %s",id)
+        port_id,ipv4 = self.get_port(id)
+        LOG.info("Got port_id %s for server_ip %s",port_id,ipv4)
+        #only need floating ip assignment if networking setup requires it - manually configure this in /etc/tempest.conf
+        if (CONF.network_feature_enabled.floating_ips and 
+            CONF.network.floating_network_name):    
+            LOG.info("Creating a new floating ip...")
+            kwargs = {
+                'floating_network_id' : CONF.network.floating_network_name,
+                'port_id' : port_id, 
+                'tenant_id' : tenant_id, 
+                'fixed_ip_address' : ipv4
+            }
+
+            if CONF.network.subnet_id:
+                kwargs['subnet_id'] = CONF.network.subnet_id
+
+            r = self.manager.floating_ips_client.create_floatingip(**kwargs)
+            name = self.manager.servers_client.show_server(id)['server']['name']
+
+            fip = r['floatingip']['floating_ip_address']
+            fip_id = r['floatingip']['id']
+            fxp = r['floatingip']['fixed_ip_address']
+
+            #just stuff for logging
+            net_name = self.manager.networks_client.show_network(r['floatingip']['floating_network_id'])['network']['name']
+
+            if CONF.identity.auth_version == 'v3':
+                project_name = self.manager.projects_client.show_project(r['floatingip']['tenant_id'])['project']['name']
+            else:
+                project_name = self.manager.tenants_client.show_tenant(r['floatingip']['tenant_id'])['tenant']['name']
+            
+            LOG.info('Created new floating ip %s on network "%s" bound to server "%s" with ip %s in project "%s"',fip,net_name,name,fxp,project_name)
+
+            ipv4 = fip
+
+        #establish ssh connection
+        LOG.info('Starting SSH connection ...')
+        LOG.info('Using ip %s, username "%s"',ipv4, CONF.validation.image_ssh_user)
+        #password is temporary measure for cirros issue, need to remove this when possible
+        client = remote_client.RemoteClient(ipv4, CONF.validation.image_ssh_user, pkey=pk, password='gocubsgo',ssh_key_type='ecdsa')
+        client.validate_authentication()
+        LOG.info('SSH succeeded!')
+        LOG.info('---------------------------')
+        LOG.info('Test finished, cleaning up...')
+
+        self.manager.servers_client.delete_server(id)
+        LOG.info('Deleted server "%s"',SERVER_NAME)
+        self.manager.keypairs_client.delete_keypair(KEYPAIR_NAME)
+        LOG.info('Deleted keypair "%s"',KEYPAIR_NAME)
+        self.manager.floating_ips_client.delete_floatingip(fip_id)
+        LOG.info('Deleted floating ip %s',fip)
