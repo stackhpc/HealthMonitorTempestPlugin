3566d214a5c6dc93bf1e1ffa0c29681a691c15da
scrungus <tyler@stackhpc.com>
2022-09-26 14:32:19 +0000

    allowed commenting of options

--- HealthMonitor_tempest_plugin/tests/scenario/test_basic.py
+++ HealthMonitor_tempest_plugin/tests/scenario/test_basic.py
@@ -2,6 +2,7 @@ from math import frexp
 from re import S
 from termios import TAB2
 from HealthMonitor_tempest_plugin.tests.base import BaseHealthCheck
+from HealthMonitor_tempest_plugin.common.utils import gen_report
 
 import sys
 
@@ -12,6 +13,17 @@ from tempest import config
 
 CONF = config.CONF
 
+import re 
+
+#setup logging to output to console
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
+
 
 class BasicTest(BaseHealthCheck):
 
@@ -21,15 +33,28 @@ class BasicTest(BaseHealthCheck):
     @testtools.testcase.attr('positive')
     def test_all_flavors_and_images(self):
 
-        if(CONF.healthmon.flavors and CONF.healthmon.images and CONF.healthmon.ssh_users):
+        regex = re.compile(r'#\S+')
 
-            for f in list(filter(None,CONF.healthmon.flavors.split('\n'))):
-                for i,ssh_user in zip(list(filter(None,CONF.healthmon.images.split('\n'))),
-                                      list(filter(None,CONF.healthmon.ssh_users.split('\n')))):
-                    self.create_server_and_get_ssh(ssh_user,i,f)
+        runs = []
+        runs_alt = []
+
+        if(CONF.healthmon.flavors and CONF.healthmon.images and CONF.healthmon.ssh_users):
+            for f in [f for f in list(filter(None,CONF.healthmon.flavors.split('\n'))) if not regex.match(f)]:
+
+                for i,ssh_user in zip([i for i in list(filter(None,CONF.healthmon.images.split('\n'))) if not regex.match(i)],
+                                      [s for s in list(filter(None,CONF.healthmon.ssh_users.split('\n'))) if not regex.match(s)]):
+
+                    success,time = self.create_server_and_get_ssh(ssh_user,i,f)
+                    runs.append((i,f,success,time))
         if(CONF.healthmon.flavors_alt and CONF.healthmon.images_alt):
-            for f in CONF.healthmon.flavors_alt.split('\n'):
-                for i in CONF.healthmon.images_alt.split('\n'):
-                    self.create_server_and_get_ssh(i,f)
+            for f in list(filter(None,CONF.healthmon.flavors_alt.split('\n'))):
+
+                for i,ssh_user in zip(list(filter(None,CONF.healthmon.images_alt.split('\n'))),
+                                      list(filter(None,CONF.healthmon.ssh_users_alt.split('\n')))):
+
+                    success,time = self.create_server_and_get_ssh(ssh_user,i,f)
+                    runs_alt.append((i,f,success,time))
+        
+        LOG.info(gen_report(runs,runs_alt))
         
         
