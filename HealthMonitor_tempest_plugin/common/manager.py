a1da440e19ecb296eade9a72c63b4932e0b4f878
scrungus <tyler@stackhpc.com>
2022-09-27 12:16:09 +0000

    deleted

--- HealthMonitor_tempest_plugin/common/manager.py
+++
@@ -1,9 +0,0 @@
-from tempest import clients
-from tempest.common import credentials_factory as common_creds
-
-class Manager(clients.Manager):
-
-    def __init__(self, credentials=None, request_type=None):
-        if not credentials:
-            credentials = common_creds.get_configured_admin_credentials(identity_version='v3')
-        super(Manager, self).__init__(credentials)
